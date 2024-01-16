14fd0dba8ab03f7ca9b3126945976bde1c5c5d26
YanivZimmer <zimmer.yaniv@gmail.com>
2024-01-16 19:20:03 +0200

    Add twitter model

--- section1.py
+++ section1.py
@@ -1,10 +1,9 @@
 import gensim.downloader as dl
-model = dl.load("word2vec-google-news-300")
+model_google = dl.load("word2vec-google-news-300")
+model_wiki = dl.load("glove-wiki-gigaword-200")
 # this will take a while on first load as it downloads a 1.6G file.
 # later calls will be cached.
 # You can now use various methods of the “model“ object.
-# you can access the vocabulary like so:
-vocab = model.index_to_key
 target_words = ["car","dog","apple","face","vice"]
 poly_words=["bank","bat","apple"]
 def similiarity_score(emb_model,word1, word2):
@@ -26,6 +25,6 @@ def most_similar_words(emb_model,words):
 w1='Happy'
 w2='Gay'
 w3='Sad'
-print("sim w1,w2",similiarity_score(model,w1,w2))
-print("sim w1,w3",similiarity_score(model,w1,w3))
+print("sim w1,w2",similiarity_score(model_google,w1,w2))
+print("sim w1,w3",similiarity_score(model_google,w1,w3))
 
